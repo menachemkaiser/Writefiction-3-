25066e29ce977fe5b2f9aece72d5318e29292e71
Robert <robert.van122@yahoo.com>
2015-11-19 09:38:21 -0800

    db configure

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	'url' : "mongodb://localhost:27017/writefiction",
+	'url' : "mongodb://heroku_n89v335w:jjgqqqoak00opbmohhmhur3ev8@ds057224.mongolab.com:57224/heroku_n89v335w",
 	'secret' : "bdwaefa33!d#"
 }
